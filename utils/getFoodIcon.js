6c322662a041d24aaedbded0cadbf26e0852d1fa
vbisso <valeriabisso19@gmail.com>
2025-06-20 11:50:31 -0600

    icons

--- utils/getFoodIcon.js
+++ utils/getFoodIcon.js
@@ -1,17 +1,20 @@
 export default function getFoodIcon(category) {
   const icons = {
-    // Protein: require("../assets/icons/Protein.png"),
-    // Fruit: require("../assets/icons/Fruit.png"),
-    // Vegetable: require("../assets/icons/Vegetable.png"),
-    // Dairy: require("../assets/icons/Dairy.png"),
+    Protein: require("../assets/icons/protein.png"),
+    Fruit: require("../assets/icons/fruits.png"),
+    Vegetable: require("../assets/icons/vegetable.png"),
+    Dairy: require("../assets/icons/dairy.png"),
     // Sweet: require("../assets/icons/sweet_icon.png"),
     // Juice: require("../assets/icons/juice_icon.png"),
-    // Condiments: require("../assets/icons/Condiments.png"),
-    // Beverages: require("../assets/icons/Beverages.png"),
+    Condiments: require("../assets/icons/condiments.png"),
+    Beverages: require("../assets/icons/beverages.png"),
     Grain: require("../assets/icons/grains.png"),
     "Oils & Fats": require("../assets/icons/oils&fats.png"),
     "Dry Goods": require("../assets/icons/dry goods.png"),
     "Nuts & Seeds": require("../assets/icons/seeds and nuts.png"),
+    "Canned Goods": require("../assets/icons/canned goods.png"),
+    Snacks: require("../assets/icons/snacks.png"),
+    "Herbs & Spices": require("../assets/icons/spices.png"),
   };
   // Fallback to a default icon if category not found
   return icons[category] || require("../assets/icons/juice_icon.png");
