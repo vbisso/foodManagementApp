5b88466e0708f4eda71b2d6a358f76e59bebc10a
Lalaine Macababbad <lalaine@MacBook-Air-7.local>
2025-07-09 12:50:05 -0600

    tried supabase

---
+++ utils/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
